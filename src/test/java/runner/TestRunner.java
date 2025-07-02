6a6e58a992e8f4a5f9b9571ce0f9f0d55f9e1750
Adrian Alvarez <adrianhae@gmail.com>
2025-07-02 12:24:17 +0200

    Refactor TestRunner to use Cucumber's JUnit platform engine and remove deprecated annotations

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,23 +1,8 @@
 package runner;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "steps",
-        plugin = {
-                "pretty",
-                "html:build/reports/cucumber.html",
-                "json:build/reports/cucumber.json",
-                "junit:build/test-results/test/cucumber.xml"
-        },
-        
-        monochrome = true,
-        tags = ""
-)
+@Cucumber
 public class TestRunner {
 }
